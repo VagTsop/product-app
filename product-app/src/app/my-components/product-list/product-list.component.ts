2ea03b79a6fb5b18ea3e3fecb55c6022cf010728
VagTsop <vatsop52@gmail.com>
2024-09-22 12:57:06 +0300

    progress product details cards

--- product-app/src/app/my-components/product-list/product-list.component.ts
+++ product-app/src/app/my-components/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
@@ -5,10 +6,31 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './product-list.component.html',
 })
 export class ProductListComponent implements OnInit, OnDestroy {
+  groupedProducts: any = {};
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.http.get<any[]>('/assets/data/product-list.json').subscribe((data) => {
+      this.groupProductsByTitle(data);
+    });
+  }
+
+  groupProductsByTitle(products: any[]): void {
+    this.groupedProducts = products.reduce((grouped, product) => {
+      const title = product.title;
+      if (!grouped[title]) {
+        grouped[title] = [];
+      }
+      grouped[title].push(product);
+      console.log(grouped)
+      return grouped;
+    }, {});
+  }
 
   ngOnDestroy() { }
 }
